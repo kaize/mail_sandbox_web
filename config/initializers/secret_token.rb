812582da753283b87fe772f321f6bb4840c554b5
Alexander Shcherbinin <realmyst@ya.ru>
2012-08-04 18:25:18 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailSandboxWeb::Application.config.secret_token = '00e7d76d0861645170359c66ad357f5637afd6d70b2d0549895e0f848c90bba0c544e14f8f9c4ad4b84c791d10b15635d8e1afd14eba5e189c87a4c056dfb350'
