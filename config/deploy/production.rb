80eff3312c3322b23169b4d6518a4e0af9d082b8
Rozhnov Alexandr <nox73@ya.ru>
2012-08-09 20:03:21 +0400

    staging user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :rails_env, :production
 set :branch, 'master'
 
-set :user, 'mail_sandbox_production'
+set :user, 'mail_sandbox_web_production'
 
 role :web, ""
 role :app, ""
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :rails_env, :staging
 set :branch, 'staging'
 
-set :user, 'mail_sandbox_staging'
+set :user, 'mail_sandbox_web_staging'
 
 role :web, "10.40.35.81"
 role :app, "10.40.35.81"
