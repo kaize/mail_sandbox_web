7d369e9c83d2a7fa8df5375b70ca69f32f351891
Rozhnov Alexandr <nox73@ya.ru>
2012-08-23 15:18:34 +0400

    refactoring MailApplicationRepository

--- app/models/mail_application.rb
+++ app/models/mail_application.rb
@@ -9,10 +9,6 @@ class MailApplication < ActiveRecord::Base
 
   validates :owner, :presence => true
 
-  scope :find_by_credentials, ->(name, password){ where(:name => name).where(:password => password) }
-  scope :ordered, ->{order('id DESC')}
-  scope :owner_is, ->(owner){ where(:owner_id => owner.id) }
-
   #TODO: members
   scope :available_for, ->(user){ owner_is(user) }
 
--- app/repositories/mail_application_repository.rb
+++ app/repositories/mail_application_repository.rb
@@ -4,6 +4,7 @@ module MailApplicationRepository
   included do
     scope :web
     scope :find_by_credentials, ->(name, password){ where(:name => name).where(:password => password) }
-    scope :ordered, ->{web.order('id DESC')}
+    scope :ordered, ->{ web.order('id DESC') }
+    scope :owner_is, ->(owner){ where(:owner_id => owner.id) }
   end
 end
