85a34466dfdae399041d3af3c172d7a739f210de
Rozhnov Alexandr <nox73@ya.ru>
2012-08-27 14:55:32 +0400

    refactoring

--- app/controllers/web/mail_messages_controller.rb
+++ app/controllers/web/mail_messages_controller.rb
@@ -1,5 +1,4 @@
 class Web::MailMessagesController < Web::ProtectedApplicationController
-  after_filter :mark_as_read, :only => [:show]
 
   def index
     @mail_application = MailApplication.find_by_id(params[:mail_application_id])
@@ -8,13 +7,9 @@ class Web::MailMessagesController < Web::ProtectedApplicationController
 
   def show
     @message = MailMessage.find params[:id]
-    @mail = Mail.new(@message.data)
-  end
-
-  private
-
-  def mark_as_read
     @message.mark_read
+
+    @mail = Mail.new(@message.data)
   end
 
 end
