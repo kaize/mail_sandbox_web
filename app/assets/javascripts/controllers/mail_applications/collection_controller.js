8c5af32d6c4dc8d622e3bd04dab01249c4b791bb
Rozhnov Alexandr <nox73@ya.ru>
2012-09-09 23:24:41 +0400

    load data on insert

--- app/assets/javascripts/controllers/mail_applications/collection_controller.js
+++ app/assets/javascripts/controllers/mail_applications/collection_controller.js
@@ -1,10 +1,14 @@
 MailSandboxWeb.MailApplicationsCollectionController = Ember.ArrayProxy.extend({
-  init: function() {
-			this._super();
+
+
+  init: function(){
+//    this.reload_content()
+  },
+
+  reload_content: function(){
 			var items = MailSandboxWeb.Store.findAll(MailSandboxWeb.MailApplication);
       this.set( 'content', items );
-      window.collection_controller = this
+  }
 
-		}
 });
 MailSandboxWeb.Controllers.MailApplicationsCollection = MailSandboxWeb.MailApplicationsCollectionController.create();
--- app/assets/javascripts/views/mail_applications/collection_view.js
+++ app/assets/javascripts/views/mail_applications/collection_view.js
@@ -1,14 +1,15 @@
 MailSandboxWeb.MailApplicationsCollectionView = Ember.CollectionView.extend({
+
   tagName: 'table',
   contentBinding: 'controller.content',
+
   itemViewClass: Ember.View.extend({
     tagName: 'tr',
     templateName: 'mail_applications/mail_application'
   }),
 
-  init: function(){
-    this._super();
-    window.collection_view = this
+  willInsertElement: function(){
+    this.get('controller').reload_content()
   }
 
 });
