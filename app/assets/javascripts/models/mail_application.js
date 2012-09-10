4b91e9920f5bab6f0ed579d0829d129bb59e89cc
Rozhnov Alexandr <nox73@ya.ru>
2012-09-10 16:25:44 +0400

    -m

--- app/assets/javascripts/models/mail_application.js
+++ app/assets/javascripts/models/mail_application.js
@@ -2,5 +2,6 @@ MailSandboxWeb.MailApplication = DS.Model.extend({
   name: DS.attr('string'),
   unread_mails_count: DS.attr('number'),
   owner_nickname: DS.attr('string'),
-  edit_link: DS.attr('string')
+  edit_link: DS.attr('string'),
+  delete_link: DS.attr('string')
 });
