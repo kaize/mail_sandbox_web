fa198860f469bd0fa52fce199150bcbcb7cd4acf
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-18 12:40:24 +0400

    respond with status ok to mark_all_messages_as_read

--- app/controllers/api/mail_applications_controller.rb
+++ app/controllers/api/mail_applications_controller.rb
@@ -1,11 +1,11 @@
 class Api::MailApplicationsController < Api::ProtectedApplicationController
 
   def mark_all_messages_as_read
-    @application = available_applications.find(params[:id])
-    @messages = @application.mail_messages
-    @messages.each { |m| m.mark_read }
+    application = available_applications.find(params[:id])
+    messages = application.mail_messages
+    messages.each { |m| m.mark_read }
 
-    respond_with @messages
+    respond_with :status => :ok
   end
 
   private
