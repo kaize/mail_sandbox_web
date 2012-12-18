36c48c240d7e465c252481b26b0721cfc04aa32a
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-18 12:09:49 +0400

    deleted all empty helpers

--- test/unit/helpers/web/mail_messages_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Web::MailMessagesHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/web/user/networks_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Web::User::NetworksHelperTest < ActionView::TestCase
-end
