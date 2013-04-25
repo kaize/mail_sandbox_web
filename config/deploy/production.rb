f12e6888fd6d5810cac3a7e925a4c4824bfd703f
Rozhnov Alexandr <nox73@ya.ru>
2013-04-25 19:59:31 +0400

    Production restart task.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,3 +17,14 @@ role :app, "192.168.249.187"
 role :db,  "192.168.249.187", :primary => true
 
 set :keep_releases, 10
+
+
+namespace :sandbox do
+  desc "Restart mail sandbox"
+  task :restart, :roles => :app do
+    run "sudo sv restart /etc/service/mail_sandbox_face"
+    run "sudo sv restart /etc/service/mail_sandbox_worker"
+  end
+end
+
+after 'deploy:restart', 'sandbox:restart'
