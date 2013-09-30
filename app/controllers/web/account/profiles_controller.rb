10e1e167527162060eefff5465005d50338883d7
Alexey Astafyev <av.astafyev@gmail.com>
2013-09-30 16:57:32 +0400

    added profile editing (refs #34378)

---
+++ app/controllers/web/account/profiles_controller.rb
@@ -0,0 +1,17 @@
+class Web::Account::ProfilesController < Web::ProtectedApplicationController
+  def edit
+    @user = current_user.becomes UserType
+  end
+
+  def update
+    @user = current_user.becomes UserType
+
+    if @user.update_attributes(params[:user])
+      flash.now[:notice] = flash_translate(:success)
+      redirect_to edit_account_profile_path
+    else
+      flash.now[:error] = flash_translate(:update_failed)
+      render :edit
+    end
+  end
+end
