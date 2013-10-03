adf6a4997d40598e1cc384cf68f84141df6fac48
Alexey Astafyev <av.astafyev@gmail.com>
2013-10-03 11:12:41 +0400

    fixed test

--- test/functional/web/user/passwords_controller_test.rb
+++ test/functional/web/user/passwords_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class Web::User::PasswordsControllerTest < ActionController::TestCase
+  def setup
+    @user = create :user
+    @user.confirm
+  end
+
   test "should get forget" do
     get :forget
     assert_response :success
   end
 
   test "should get reset" do
-    params = {user_reset_password_type: attributes_for(:user)}
+    params = {user_reset_password_type: @user.attributes}
     get :reset, params
     assert_response :redirect
   end
