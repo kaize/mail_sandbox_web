f7e056b93a007a2ed3050afc60e9275a1e326826
Paul Yakshankin <yakspavel@undev.ru>
2013-10-04 13:07:29 +0000

    added scopes to messages

--- app/repositories/mail_message_repository.rb
+++ app/repositories/mail_message_repository.rb
@@ -5,5 +5,9 @@ module MailMessageRepository
     scope :web, ->{ without_state(:deleted) }
     scope :ordered, ->{ web.order('id DESC') }
     scope :unreaded, ->{ with_state(:unread) }
+
+    scope :uniq_by_recipients, ->{ select(:recipient).uniq }
+    scope :uniq_by_senders,    ->{ select(:sender).uniq }
+
   end
 end
