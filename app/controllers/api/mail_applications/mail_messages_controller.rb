dd3c94fe9dd18669f1f621fa92ca37a9dea68c6f
Paul Yakshankin <yakspavel@undev.ru>
2013-10-18 11:46:34 +0400

    added mail_messages api controller

---
+++ app/controllers/api/mail_applications/mail_messages_controller.rb
@@ -0,0 +1,14 @@
+class Api::MailApplications::MailMessagesController < Api::MailApplications::ApplicationController
+
+  def index
+    @messages = resource_application.mail_messages.ordered.page(params[:page]).per(params[:per_page])
+
+    meta = {
+      total_pages: @messages.total_pages,
+      per_page: params[:per_page] || Kaminari.config.default_per_page
+    }
+
+    respond_with @messages
+  end
+
+end
