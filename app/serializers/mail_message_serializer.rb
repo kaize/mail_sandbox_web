7d662fb59cdf06318dc128a26e14542ed26c644f
Paul Yakshankin <yakspavel@undev.ru>
2013-10-23 15:02:01 +0400

    extended mail_message serializer

--- app/serializers/mail_message_serializer.rb
+++ app/serializers/mail_message_serializer.rb
@@ -1,9 +1,13 @@
 class MailMessageSerializer < ActiveModel::Serializer
   attributes :id, :recipient, :sender, :subject, :body,
-    :state, :completed_at
+    :state, :completed_at, :mail_application
 
   def body
     object.utf8body
+  end
+
+  def mail_application
+    object.mail_application.name
   end
 
 end
