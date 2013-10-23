2ff8c8734d2db82eb8bff1ca6fe3f915a2ed2e71
Paul Yakshankin <yakspavel@undev.ru>
2013-10-23 09:45:22 +0400

    added state to mail_message serializer

--- app/serializers/mail_message_serializer.rb
+++ app/serializers/mail_message_serializer.rb
@@ -1,5 +1,6 @@
 class MailMessageSerializer < ActiveModel::Serializer
-  attributes :id, :recipient, :sender, :subject, :body, :completed_at
+  attributes :id, :recipient, :sender, :subject, :body,
+    :state, :completed_at
 
   def body
     object.utf8body
