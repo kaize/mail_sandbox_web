4d338e8df647e8314c49a1f9030cd3f791fd9c64
Paul Yakshankin <yakspavel@undev.ru>
2013-11-08 16:32:00 +0400

    fixed angular urls

--- lib/angular_url_helpers.rb
+++ lib/angular_url_helpers.rb
@@ -5,15 +5,15 @@ module AngularUrlHelpers
   end
 
   def self.mail_application_path(application)
-    "#/mail_applications/#{application.id}"
+    "/#/mail_applications/#{application.id}"
   end
 
   def self.new_mail_application_path
-    "#/mail_applications/new"
+    "/#/mail_applications/new"
   end
 
   def self.mail_message_path(message)
-    "#/mail_applications/#{message.mail_application_id}/mail_messages/#{message.id}/without_bootstrap"
+    "/#/mail_applications/#{message.mail_application_id}/mail_messages/#{message.id}/raw"
   end
 
 end
