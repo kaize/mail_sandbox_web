6f14dc670666a6f84905f235d3fb29a8e12642b5
Paul Yakshankin <yakspavel@undev.ru>
2013-11-11 17:35:36 +0400

    fixed mail_message repository

--- app/repositories/mail_message_repository.rb
+++ app/repositories/mail_message_repository.rb
@@ -3,11 +3,11 @@ module MailMessageRepository
 
   included do
     scope :web, ->{ without_state(:deleted) }
-    scope :ordered, ->{ web.order('id DESC') }
+    scope :ordered, ->{ web.order('completed_at DESC') }
     scope :unreaded, ->{ with_state(:unread) }
 
     scope :last_minute, -> { where('created_at >= ?', 1.minute.ago.utc) }
-    scope :recent, -> { ordered.limit(20) }
+    scope :recent, ->(count = 10) { ordered.limit(count) }
 
     scope :uniq_by_recipients, ->{ select(:recipient).distinct }
     scope :uniq_by_senders,    ->{ select(:sender).distinct }
