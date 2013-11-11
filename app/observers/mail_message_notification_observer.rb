bfa902b81699d9e1d72d8db653acb44ed2f2af60
Paul Yakshankin <yakspavel@undev.ru>
2013-11-11 12:13:24 +0400

    fixed observer

--- app/observers/mail_message_notification_observer.rb
+++ app/observers/mail_message_notification_observer.rb
@@ -1,8 +1,10 @@
 class MailMessageNotificationObserver < ActiveRecord::Observer
   observe :mail_message
 
-  def after_create(message)
-    FayeService.mail_message_new(message)
+  def after_commit(message)
+    if message.send(:transaction_include_any_action?, [:create])
+      FayeService.mail_message_new(message)
+    end
   end
 
 end
