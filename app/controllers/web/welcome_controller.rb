6f572e72d92c12650accf4ab7d27d4fc684127e8
Paul Yakshankin <yakspavel@undev.ru>
2013-12-21 15:07:53 +0400

    добавил появление приложения в правом блоке на welcome странице(refs #38579)

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -1,14 +1,26 @@
 class Web::WelcomeController < Web::ProtectedApplicationController
+  APPS_NUMBER_ON_PAGE = 10
+
   def index
-    @mail_applications = available_applications
-    @recent_mail_applications = @mail_applications.recently_active.decorate
+    @avaliable_apps = available_apps
+
+    @recently_active_apps = @avaliable_apps.recently_active(APPS_NUMBER_ON_PAGE).decorate
+    recently_active_apps_count = @recently_active_apps.count
+
+    if @recently_active_apps.count < APPS_NUMBER_ON_PAGE
+      @apps_without_messages = apps_without_messages(APPS_NUMBER_ON_PAGE - recently_active_apps_count)
+    end
 
     @messages = current_user.admin ? MailMessage.recent : MailMessage.recent_by_user(current_user)
   end
 
   private
 
-    def available_applications
+    def available_apps
       current_user.available_applications
+    end
+
+    def apps_without_messages(count)
+      @avaliable_apps.without_messages(count).decorate
     end
 end
--- app/repositories/mail_application_repository.rb
+++ app/repositories/mail_application_repository.rb
@@ -10,6 +10,7 @@ module MailApplicationRepository
     scope :available_for, ->(user){ where{ ( creator_id == user.id ) | ( id >> user.membered_application_ids ) } }
 
     scope :recently_active, ->(count = 10) { where("last_message_at IS NOT NULL").order(last_message_at: :desc).limit(count) }
+    scope :without_messages, ->(count = 10) { where("last_message_at IS NULL").order(created_at: :desc).limit(count) }
   end
 
 end
